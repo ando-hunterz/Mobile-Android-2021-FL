7279d997e4ce1d75a5ee4930cab81ebb2d2538b5
ando-hunterz <ando.hunterz@gmail.com>
2021-04-26 20:45:49 +0700

    Add Week 11

---
+++ Week11/app/src/main/java/id/ac/umn/week11/JsonPlaceholderApi.java
@@ -0,0 +1,12 @@
+package id.ac.umn.week11;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceholderApi {
+
+    @GET("posts")
+    Call<List<Post>> getPosts();
+}
